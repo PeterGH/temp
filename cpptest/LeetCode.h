a93f85a9cd77fb7c8561e4ad4d1a8d907cf67bb5
PeterGH <peteryzufl@gmail.com>
2018-11-10 10:12:20 -0800

    test

--- cpptest/LeetCode.h
+++ cpptest/LeetCode.h
@@ -177,44 +177,7 @@ namespace Test {
             
             
             
-            // 88. Merge Sorted Array
-            // Given two sorted integer arrays nums1 and nums2, merge nums2 into nums1 as one sorted array.
-            // Note:
-            // You may assume that nums1 has enough space(size that is greater or equal to m + n)
-            // to hold additional elements from nums2. The number of elements initialized in nums1 and nums2
-            // are m and n respectively.
-            static void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-                vector<int>::iterator it1 = nums1.begin();
-                vector<int>::iterator it2 = nums2.begin();
-                int i = 0;
-                int j = 0;
-                while (i < m || j < n) {
-                    if (i < m && j < n) {
-                        if (*it1 <= *it2) {
-                            it1++;
-                            i++;
-                        }
-                        else {
-                            it1 = nums1.insert(it1, *it2);
-                            it1++;
-                            it2 = nums2.erase(it2);
-                            j++;
-                        }
-                    }
-                    else if (j < n) {
-                        it1 = nums1.insert(it1, *it2);
-                        it1++;
-                        it2 = nums2.erase(it2);
-                        j++;
-                    }
-                    else {
-                        it1++;
-                        i++;
-                    }
-                }
-                while (it1 != nums1.end()) it1 = nums1.erase(it1);
-            }
-
+            
             // 89. Gray Code
             // The gray code is a binary numeral system where two successive values differ in only one bit.
             // Given a non-negative integer n representing the total number of bits in the code,
